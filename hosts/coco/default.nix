ea47cdb6f84df3c341bdc20448868ba4f27a1317
Anthony Loria <anthony@anthonyloria.dev>
2026-01-31 11:24:43 -0700

    optimized

--- hosts/coco/default.nix
+++ hosts/coco/default.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
   pkgs,
   ...
@@ -11,24 +8,38 @@
     ./boot.nix
   ];
 
-  networking.hostName = "coco"; # Define your hostname.
+  networking.hostName = "coco";
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix.settings = {
+    # Enable flakes
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+
+    # Build performance
+    max-jobs = "auto";
+    cores = 0;
+
+    # Reduce disk usage
+    keep-outputs = false;
+    keep-derivations = false;
+    auto-optimise-store = true;
+
+    # Binary cache (prevents massive local builds)
+    substituters = [
+      "https://cache.nixos.org"
+    ];
+
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+    ];
+  };
 
   environment.systemPackages = with pkgs; [
-    # add global basic packages here (the ones that are used quite literally everywhere)
     wget
     curl
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
+  system.stateVersion = "24.11";
 }
