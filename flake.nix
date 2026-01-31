8010a00b161f1a562f51fd46d8faaeb705d0f966
Anthony Loria <anthony@anthonyloria.dev>
2026-01-31 12:08:18 -0700

    test

--- flake.nix
+++ flake.nix
@@ -2,24 +2,118 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    # ... other inputs
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+    nixpkgs-stable = {
+      url = "github:nixos/nixpkgs/nixos-25.11";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    stylix = {
+      url = "github:nix-community/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    awww = {
+      url = "git+https://codeberg.org/LGFae/awww";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    affinity-nix = {
+      url = "github:mrshmllow/affinity-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    # niri-blurry = {
+    #   type = "github";
+    #   owner = "visualglitch91";
+    #   repo = "niri";
+    #   ref = "feat/blur";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    # niri-unstable = {
+    #   type = "github";
+    #   owner = "YaLTeR";
+    #   repo = "niri";
+    #   ref = "main";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+
+    nh = {
+      url = "github:nix-community/nh";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    helix = {
+      type = "github";
+      owner = "helix-editor";
+      repo = "helix";
+      ref = "master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    ghostty = {
+      type = "github";
+      owner = "ghostty-org";
+      repo = "ghostty";
+      ref = "main";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-vscode-extensions = {
+      url = "github:nix-community/nix-vscode-extensions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    vicinae = {
+      url = "github:vicinaehq/vicinae";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    _1password = {
+      url = "github:1Password/shell-plugins";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    graphite = {
+      url = "github:GraphiteEditor/Graphite?dir=.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zed = {
+      url = "github:zed-industries/zed";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... } @ inputs: {
-
+  outputs = {
+    nixpkgs,
+    ...
+  } @ inputs: {
+    # use "nixos", or your hostname as the name of the configuration
+    # it's a better practice than "default" shown in the video
     nixosConfigurations = builtins.listToAttrs (map (name: {
       name = name;
       value = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
         modules = [
-          ./hosts/${name}
-
-          # Module to disable xdg-desktop-portal tests and move build dir
-          ({ config, pkgs, ... }: {
-
+          ({ ... }: {
             nixpkgs.overlays = [
               (final: prev: {
                 xdg-desktop-portal = prev.xdg-desktop-portal.overrideAttrs (_: {
@@ -28,7 +122,14 @@
               })
             ];
           })
+
+          ./hosts/${name}
         ];
+
+        specialArgs = {
+          inherit inputs;
+          system = "x86_64-linux";
+        };
       };
     }) ["coco"]);
   };
